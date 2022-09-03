3801ea82c119f0d86f110915a0fbacd7aae41af4
Jr Bytes <itamazor@gmail.com>
2022-09-03 13:54:35 -0300

    added authenticate user to compare password and generate a token

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
 
-import { ICreateUserDTO } from '../dtos/ICreateUserDTO'
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO'
 import { CreateUserUseCase } from './CreateUserUseCase'
 
 class CreateUserController {
