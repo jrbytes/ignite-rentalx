0592a74996e4f69e6268975f7f5042d6e976469d
Jr Bytes <itamazor@gmail.com>
2022-09-03 00:01:25 -0300

    implemented find by email, updated create user to verify has exists an user with same email

--- src/modules/accounts/useCases/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs'
 import { inject, injectable } from 'tsyringe'
 
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO'
@@ -11,6 +12,14 @@ class CreateUserUseCase {
   ) {}
 
   async execute(data: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(data.email)
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists')
+    }
+
+    const passwordHash = await hash(data.password, 8)
+    Object.assign(data, { password: passwordHash })
     await this.usersRepository.create(data)
   }
 }
