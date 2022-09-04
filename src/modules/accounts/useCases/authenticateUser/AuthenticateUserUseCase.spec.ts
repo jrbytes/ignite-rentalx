b20819ca475f4b609f11429e092edafef476edc7
Jr Bytes <itamazor@gmail.com>
2022-09-04 16:10:00 -0300

    added jest to use tests, added tests to create category and authenticate user

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,65 @@
+import { faker } from '@faker-js/faker'
+
+import { AppError } from '../../../../errors/AppError'
+import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory'
+import { CreateUserUseCase } from '../createUser/CreateUserUseCase'
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase'
+
+let authenticateUser: AuthenticateUserUseCase
+let usersRepositoryInMemory: UsersRepositoryInMemory
+let createUser: CreateUserUseCase
+
+describe('Authenticate User', () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory()
+    createUser = new CreateUserUseCase(usersRepositoryInMemory)
+    authenticateUser = new AuthenticateUserUseCase(usersRepositoryInMemory)
+  })
+
+  it('should be able to authenticate an user', async () => {
+    const email = faker.name.fullName()
+    const password = faker.random.alphaNumeric(8)
+
+    await createUser.execute({
+      name: 'John Doe',
+      email,
+      password,
+      driver_license: '000123',
+    })
+
+    const authenticated = await authenticateUser.execute({
+      email,
+      password,
+    })
+
+    expect(authenticated).toHaveProperty('token')
+  })
+
+  it('should not be able to authenticate an nonexistent user', async () => {
+    expect(async () => {
+      await authenticateUser.execute({
+        email: 'nonexistent@example.com',
+        password: '1234',
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+
+  it('should not be able to authenticate with incorrect password', async () => {
+    const email = faker.name.fullName()
+    const password = faker.random.alphaNumeric(8)
+
+    await createUser.execute({
+      name: 'John Doe',
+      email,
+      password,
+      driver_license: '000123',
+    })
+
+    expect(async () => {
+      await authenticateUser.execute({
+        email,
+        password: 'incorrect',
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+})
---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,40 @@
+import { AppError } from '../../../../errors/AppError'
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory'
+import { CreateCategoryUseCase } from './CreateCategoryUseCase'
+
+let createCategory: CreateCategoryUseCase
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
+
+describe('create category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
+    createCategory = new CreateCategoryUseCase(categoriesRepositoryInMemory)
+  })
+
+  it('should be able to create a new category', async () => {
+    await createCategory.execute({
+      name: 'Category test',
+      description: 'Category description test',
+    })
+
+    const category = await categoriesRepositoryInMemory.findByName(
+      'Category test'
+    )
+
+    expect(category).toHaveProperty('id')
+  })
+
+  it('should not be able to create a new category with same name', async () => {
+    await createCategory.execute({
+      name: 'Category test',
+      description: 'Category description test',
+    })
+
+    expect(async () => {
+      await createCategory.execute({
+        name: 'Category test',
+        description: 'Category description test',
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+})
